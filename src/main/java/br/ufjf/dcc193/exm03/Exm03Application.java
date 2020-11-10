b1f38046c2f9f6f66ad04bcb709839ba3c05b769
dsnitram <diegonwb@gmail.com>
2020-11-10 16:47:27 -0300

    Cria projeto inicial

---
+++ src/main/java/br/ufjf/dcc193/exm03/Exm03Application.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.exm03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exm03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exm03Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufjf/dcc193/exm03/Exm03ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.exm03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exm03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
