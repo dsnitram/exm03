278c5c02048d842f9f90e5d5c509df64bcb93d85
dsnitram <diegonwb@gmail.com>
2020-11-25 18:01:58 -0300

    Adiciona implementação de usuario  inicial

---
+++ src/main/java/br/ufjf/dcc193/exm03/login/model/UsuarioDAOImpl.java
@@ -0,0 +1,50 @@
+package br.ufjf.dcc193.exm03.login.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class UsuarioDAOImpl implements UsuarioDAO {
+  
+  @Autowired
+  JdbcTemplate jdcb;
+  @Override
+  public int count() throws DataAccessException {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int insertOne(Usuario usuario) throws DataAccessException {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public Usuario selectOne(String username) throws DataAccessException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public int updateOne(Usuario usuario) throws DataAccessException {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int deleteOne(Usuario usuario) throws DataAccessException {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public List<Usuario> SelectAll() throws DataAccessException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  
+}
