2323eab9f71a45a177799f9baa8b2cd069f2f63d
dsnitram <diegonwb@gmail.com>
2020-11-25 17:29:55 -0300

    Adiciona classe  de modelo para usuário

---
+++ src/main/java/br/ufjf/dcc193/exm03/login/model/Usuario.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc193.exm03.login.model;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Usuario {
+private String username;
+private String fullname;
+private String password;
+private Date birthday;
+private Integer pets;
+private boolean status;  
+}
