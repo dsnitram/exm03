ef6b23b7d4cf360f02269d8403b25827d1ece924
dsnitram <diegonwb@gmail.com>
2020-11-25 18:50:02 -0300

    Cria serviço para usuario

---
+++ src/main/java/br/ufjf/dcc193/exm03/login/model/UsuarioService.java
@@ -0,0 +1,12 @@
+package br.ufjf.dcc193.exm03.login.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+ @Service
+public class UsuarioService {
+  @Autowired
+  private UsuarioDAO dao; {    
+  }
+  
+}
